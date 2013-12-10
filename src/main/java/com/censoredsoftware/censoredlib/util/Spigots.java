555c4a6844a4e10cce2c3835d5a47f9e21f694e4
HmmmQuestionMark <ajchauncey@gmail.com>
2013-12-09 20:01:00 -0500

    Fix for spigot API not being finished yet.

--- src/main/java/com/censoredsoftware/censoredlib/util/Spigots.java
+++ src/main/java/com/censoredsoftware/censoredlib/util/Spigots.java
@@ -7,6 +7,11 @@ public class Spigots
 {
 	public static void playParticle(Location location, Effect effect, int offsetX, int offsetY, int offsetZ, float speed, int particles, int viewRadius)
 	{
-		location.getWorld().spigot().playEffect(location, effect, 1, 1, offsetX, offsetY, offsetZ, speed, particles, viewRadius);
+		try
+		{
+			location.getWorld().spigot().playEffect(location, effect, 1, 1, offsetX, offsetY, offsetZ, speed, particles, viewRadius);
+		}
+		catch(Exception ignored)
+		{}
 	}
 }
