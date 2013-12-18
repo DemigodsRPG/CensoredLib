ae65bfc447b566d21a235124c38e75c761bd6565
HmmmQuestionMark <ajchauncey@gmail.com>
2013-12-18 18:46:40 -0500

    Don't know what I was thinking.

--- CensoredLib/src/main/java/com/censoredsoftware/censoredlib/helper/CensoredCentralizedClass.java
+++ CensoredLib/src/main/java/com/censoredsoftware/censoredlib/helper/CensoredCentralizedClass.java
@@ -2,8 +2,6 @@ package com.censoredsoftware.censoredlib.helper;
 
 public abstract class CensoredCentralizedClass
 {
-	public abstract CensoredJavaPlugin inst();
-
 	protected abstract int loadWorlds();
 
 	protected abstract void loadListeners();
