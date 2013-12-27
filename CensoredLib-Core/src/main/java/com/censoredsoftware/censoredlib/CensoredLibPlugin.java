92d301b7495e9fc3ca7412713a5fdfde1737054d
HmmmQuestionMark <ajchauncey@gmail.com>
2013-12-26 21:45:21 -0500

    Small adjustment to ascii art.

--- CensoredLib-Core/src/main/java/com/censoredsoftware/censoredlib/CensoredLibPlugin.java
+++ CensoredLib-Core/src/main/java/com/censoredsoftware/censoredlib/CensoredLibPlugin.java
@@ -38,12 +38,12 @@ public class CensoredLibPlugin extends JavaPlugin
 	private void message(String status)
 	{
 		getLogger().info("  ");
-		getLogger().info("             888        d8b   888");
-		getLogger().info("             888              888");
-		getLogger().info("   .d8888b   888        888   88888b.");
-		getLogger().info("  d88P\"      888        888   888 \"88b");
-		getLogger().info("  Y88b.      888        888   888 d88P");
-		getLogger().info("   \"Y8888P   88888888   888   88888P\"");
+		getLogger().info("            888        d8b   888");
+		getLogger().info("            888              888");
+		getLogger().info("  .d8888b   888        888   88888b.");
+		getLogger().info(" d88P\"      888        888   888 \"88b");
+		getLogger().info(" Y88b.      888        888   888 d88P");
+		getLogger().info("  \"Y8888P   88888888   888   88888P\"");
 		getLogger().info("  ");
 		getLogger().info(" ...version " + getDescription().getVersion().replaceAll("SNAPSHOT", "S") + " has " + status + " successfully!");
 	}
