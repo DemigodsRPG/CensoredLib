ecc02b2d600a40d49e8d4ee5bdf433c9e08ef8c6
HmmmQuestionMark <ajchauncey@gmail.com>
2014-01-02 12:49:17 -0500

    Update WrappedCommand.

--- CensoredLib-Core/src/main/java/com/censoredsoftware/censoredlib/helper/WrappedCommand.java
+++ CensoredLib-Core/src/main/java/com/censoredsoftware/censoredlib/helper/WrappedCommand.java
@@ -1,10 +1,15 @@
 package com.censoredsoftware.censoredlib.helper;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
@@ -12,14 +17,34 @@ public abstract class WrappedCommand implements TabExecutor
 {
 	public WrappedCommand(CensoredJavaPlugin plugin, boolean tab)
 	{
-		for(String command : getCommands())
+		for(String command : getCommandNames())
 		{
 			plugin.getCommand(command).setExecutor(this);
 			if(tab) plugin.getCommand(command).setTabCompleter(this);
 		}
 	}
 
-	public abstract Set<String> getCommands();
+	public abstract Set<String> getCommandNames();
+
+	public Set<String> getAliases()
+	{
+		Set<String> set = Sets.newHashSet();
+		for(Command command : getCommands())
+			set.addAll(command.getAliases());
+		return set;
+	}
+
+	public Collection<Command> getCommands()
+	{
+		return Collections2.transform(getCommandNames(), new Function<String, Command>()
+		{
+			@Override
+			public Command apply(String name)
+			{
+				return Bukkit.getPluginCommand(name);
+			}
+		});
+	}
 
 	public abstract boolean processCommand(CommandSender sender, Command command, final String[] args);
 
