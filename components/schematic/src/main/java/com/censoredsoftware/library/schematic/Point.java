dff9ddb4ca768237030c0da56832bfc16db1e544
HmmmQuestionMark <ajchauncey@gmail.com>
2014-12-23 00:12:22 -0500

    Update point to fix equality problems.

--- components/schematic/src/main/java/com/censoredsoftware/library/schematic/Point.java
+++ components/schematic/src/main/java/com/censoredsoftware/library/schematic/Point.java
@@ -1,5 +1,7 @@
 package com.censoredsoftware.library.schematic;
 
+import java.util.Objects;
+
 public class Point {
     private final int X, Y, Z;
     private final World world;
@@ -29,5 +31,15 @@ public class Point {
 
     public Point add(int X, int Y, int Z) {
         return new Point(this.X + X, this.Y + Y, this.Z + Z, world);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Point && ((Point) obj).X == X && ((Point) obj).Y == Y && ((Point) obj).Z == Z && ((Point) obj).world.getName().equals(world.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(X, Y, Z, world.getName());
     }
 }
